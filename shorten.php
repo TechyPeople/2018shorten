84def2da0c92b079ff2c69403832710ad4566f52
Sanil Chawla <hack@sanil.co>
2015-07-02 20:04:47 -0700

    lets steal code from another app i made

--- shorten.php
+++ shorten.php
@@ -1,10 +1,23 @@
 <?php
-    $key = $_REQUEST['key'];
-    $url = $_REQUEST['url'];
+    $newKey = $_REQUEST['key'];
+    $newVal = $_REQUEST['url'];
+    $error = "Error!  " . $newKey . " couldn't be added.  Make sure you've added a valid link (with the http:// or https:// prefix) and then try again.";
     
-    $list = json_decode(file_get_contents('list.json'), true);
+    if(strpos($newVal, 'http') !== false) {
+        $list = json_decode(file_get_contents("list.json"), true);
 
-    $list[$key] = $url;
+        $list[$newKey] = $newVal;
 
-    file_put_contents(json_encode($list));
+        $newList = json_encode($list);
+        file_put_contents("list.json", $newList);
+        
+        if(json_decode(file_get_contents("list.json"), true)[$key] == $value) {
+            echo( $newKey . " has been added, with the corresponding URL being " . $newVal . ".  Thanks!");
+        } else {
+            echo($error);
+        }
+        
+    } else {
+        echo($error);
+    }
 ?>
